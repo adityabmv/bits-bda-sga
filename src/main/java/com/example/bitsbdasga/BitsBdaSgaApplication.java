06bf1a7e0142537571598e04b3beb4926308a5dc
Aditya <adityaburgula123@gmail.com>
2025-05-06 15:06:51 +0530

    setup: initial project setup

---
+++ src/main/java/com/example/bitsbdasga/BitsBdaSgaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitsbdasga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitsBdaSgaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitsBdaSgaApplication.class, args);
+    }
+
+}
